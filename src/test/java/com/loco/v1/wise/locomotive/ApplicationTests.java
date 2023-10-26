e71c2516a4ae3cec33add485662338d6983d4bd0
Himanshu Singour <singourhimanshu3@gmail.com>
2023-10-26 11:52:11 +0530

    Railway Application

---
+++ src/test/java/com/loco/v1/wise/locomotive/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loco.v1.wise.locomotive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
